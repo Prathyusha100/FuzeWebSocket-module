7d3e71fa4e573d61293402c1c2e2ba2958bf42e3
Prathyusha Thangella <prathyusha.t@BRILLIO.COM>
2020-04-16 11:43:26 +0530

    websocketapp added

---
+++ FuzeAppWebsocket/src/main/java/com/websocket/FuzeAppWebsocket/FuzeAppWebsocketApplication.java
@@ -0,0 +1,13 @@
+package com.websocket.FuzeAppWebsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuzeAppWebsocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuzeAppWebsocketApplication.class, args);
+	}
+
+}
